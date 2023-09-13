c4c943973a853af8ecbfd21074d0fd84bd99ba39
lucianokoifi <luciano@kondor.finance>
2023-09-13 01:24:29 -0300

    styles: add themes

---
+++ src/theme/darkPalette.ts
@@ -0,0 +1,65 @@
+const darkPalette = {
+  text: {
+    primary: '#FFFFFF',
+    secondary: '#636669',
+    disabled: '#636669',
+  },
+  primary: {
+    dark: '#468C84',
+    main: '#6AD6CA',
+    light: '#7CF2E5',
+  },
+  secondary: {
+    dark: '#FF0000',
+    main: '#FFFFFF',
+    light: '#12FF80',
+    background: '#303033',
+  },
+  border: {
+    main: '#636669',
+    light: '#303033',
+    background: '#030B0E',
+  },
+  error: {
+    dark: '#AC2C3B',
+    main: '#FF5F72',
+    light: '#FFB4BD',
+    background: '#2F2527',
+  },
+  success: {
+    dark: '#028D4C',
+    main: '#00B460',
+    light: '#81C784',
+    background: '#1F2920',
+  },
+  info: {
+    dark: '#52BFDC',
+    main: '#5FDDFF',
+    light: '#B7F0FF',
+    background: '#FF0000',
+  },
+  warning: {
+    dark: '#CD674E',
+    main: '#FF8061',
+    light: '#FFB7A6',
+    background: '#2F2318',
+  },
+  background: {
+    default: '#030B0E',
+    main: '#030B0E',
+    paper: '#10171A',
+    light: '#1B2A22',
+  },
+  backdrop: {
+    main: '#636669',
+  },
+  logo: {
+    main: '#FFFFFF',
+    background: '#303033',
+  },
+  static: {
+    main: '#030B0E',
+  },
+}
+
+export default darkPalette
